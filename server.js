dcd1aefeb0f75b83e7c4fc8532e5acbd32a789cc
hamilton moore <hamlmoor@gmail.com>
2017-08-31 12:44:02 -0500

    fixed delete buttons for post requests

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
-const data = require("./data.js");
 const port = process.env.PORT || 8000;
 const app = express();
-
-let user; // I don't have a DB so I'm just making a global var.
 
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
@@ -16,9 +13,9 @@ const models = require("./models");
 app.use(express.static(path.join(__dirname, "./public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-    res.render('home', { todo: data.todo, markoff: data.markoff });  //the key matches whats in mustache
-});
+// app.get("/", function (req, res) {
+//     res.render('home', { todo: data.todo, markoff: data.markoff });  //the key matches whats in mustache
+// });
 
 // app.get("/", function (req, res) {
 //     res.render('home', { todo: data.markoff });  //the key matches whats in mustache
@@ -43,45 +40,61 @@ app.get("/", function (req, res) {
 // })
 
 //sequelize
+// models.todos.findOne().then(function (todos) {
+//     console.log(todos);
+// })
 
-models.todos.findOne().then(function (todos) {
-    console.log(todos);
-})
-
-const todo = models.todos.build(
-    {
-        task: req.body.task,
-        completed: false
-    });
-
-todo.save().then(function (todo) {
-    console.log('name is: ', newPerson.name, ' zip = ', newPerson.zip);
-});
 
 //ending sequelization //begin routes 
 
 app.get("/", (req, res) => {
-    res.render("todo", { todo: data.todo, markoff: data.markoff });
+    models.todos.findAll().then(function (foundTodos) {
+        res.render("home", { todo: foundTodos }); //todo because of mustache
+    });
 });
 
-app.post("/complete/:name", (req, res) => {
-    let task = req.params.task;
-    let index = data.todo.findIndex(function (item) { return item.task === task });
-    let targetTodo = data.todo[index];
-    targetTodo.completed = !targetTodo.completed;
-    data.markoff.push(targetTodo);
-    data.todo.splice(index, 1);
-    console.log('todos: ', data.todo, data.markoff);
-    return res.redirect("/");
+app.post("/complete/:task", (req, res) => {
+    let reqTask = req.params.task;
+    models.todos.update({ completed: true },
+        { where: { task: reqTask } }).then(function () {
+            res.redirect("/");
+        })
 });
+
 
 app.post("/todo", (req, res) => {
-    let newTodo = req.body;
-    newTodo.completed = false;
-    data.todo.push(newTodo);
-    return res.redirect("/");
+    const todo = models.todos.build(
+        {
+            task: req.body.task,
+            completed: false
+        });
+    todo.save().then(function () {
+        return res.redirect("/");
+    });
 });
 
+//delete all
+app.post("/deleteallcompleted", (req, res) => {
+    models.todos.destroy({
+        where: {
+            completed: true
+        }
+    }).then(function () {
+        return res.redirect("/");
+    })
+});
+
+//delete an individual task
+app.post("/deleteindividual/:task", (req, res) => {
+    let reqTask = req.params.task;
+    models.todos.destroy({
+        where: {
+            task: reqTask
+        }
+    }).then(function () {
+        return res.redirect("/");
+    })
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
